5b04a8508dfec621b210533e7775a9070709b107
Mohamed Khaled <mohamedk98498@gmail.com>
2023-07-10 16:11:03 +0300

    init commit

---
+++ src/test/java/com/demo/pgExample/PgExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.pgExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
